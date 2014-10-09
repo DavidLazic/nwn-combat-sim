981051c79666ccead69306085c0dfb058bee58f7
David Lazic <incognetii@gmail.com>
2014-10-10 00:05:02 +0200

    checking current - set values

--- js/main.js
+++ js/main.js
@@ -6,48 +6,36 @@ SIMULATION = (function($){
 
 	var self = {};
 
+	self.valueNotSet = '<li>You need to set AC value first</li>',
+	self.valueNaN = '<li>AC value must be a number</li>',
+	self.valueRange = '<li>AC value must be between 1 and 99</li>',
+	self.fightStarted = '<li>Fight Started!</li>',
+
 	self.init = function(){
 		this.globalDOM();
 		this.bindEvents();
 	},
 
 	self.globalDOM = function(){
+		this.$set = $('.btn-set');
 		this.$fight = $('.btn-fight');
 		this.$reportLog = $('.report-log');
 	},
 
 	self.bindEvents = function(){
-		this.$fight.on('click', $.proxy(this, "simulateFight"));
+		this.$fight.on('click', $.proxy(this, "startFight"));
+		this.$set.on('click', $.proxy(this, "setValue"));
 	},
 
-	self.simulateFight = function(){
-		var inputLength = $('.hero-ac-input').val().length,
-			inputValue = parseInt($('.hero-ac-input').val());
-
-		this.validateInput(inputLength, inputValue);
+	self.startFight = function(){
+		this.validateInput(this.hero());
 	},
 
-	self.validateInput = function(length, value){
+	self.validateInput = function(obj){
 
-		var valueNotSet = '<li>You need to set AC value first</li>',
-			valueNaN = '<li>AC value must be a number</li>',
-			valueRange = '<li>AC value must be between 1 and 99</li>',
-			fightStarted = '<li>Fight Started!</li>';
-
-		// if empty
-		if(length == 0){
-			this.globalMessage(valueNotSet);
-		}else{
-			// if not a number
-			if(isNaN(value)){
-				this.globalMessage(valueNaN);
-			// if less than 0 or higher than 99
-			}else if(value <= 0 || value >= 100){
-				this.globalMessage(valueRange);
-			}else{
-				this.globalMessage(fightStarted);
-				this.$fight.addClass('disabled');
-			}
+		if(obj.checkInput() == true){
+			self.globalMessage(self.fightStarted);
+			self.$fight.addClass('disabled');
 		}
 	},
 
@@ -55,24 +43,40 @@ SIMULATION = (function($){
 		this.$reportLog.html(msg);
 	},
 
-	self.hero = function(){
-		var $hero = {};
+	self.setValue = function(){
 
-		hero.ac = null;
-
-		hero.setAC = function(){
-
-		}
-
-		return $hero;
 	},
 
-	self.opponent = function(){
-		var $opponent = {};
+	self.hero = function(){
+		var hero = {};
 
-		hero.ac = null;
+		hero.ac = {
+			max: 100,
+			min: 0,
+			currentValue: null
+		},
 
-		return $opponent;
+		hero.checkInput = function(){
+			var inputValue = parseInt($('.ac-input').val()),
+				length = $('.ac-input').val().length;
+
+			// if empty
+			if(length == 0){
+				self.globalMessage(self.valueNotSet);
+			}else{
+				// if not a number
+				if(isNaN(inputValue)){
+					self.globalMessage(self.valueNaN);
+				// if less than 0 or higher than 99
+				}else if(inputValue <= this.ac.min || inputValue >= this.ac.max){
+					self.globalMessage(self.valueRange);
+				}else{
+					return true;
+				}
+			}
+		}
+
+		return hero;
 	}
 
 	return self;
